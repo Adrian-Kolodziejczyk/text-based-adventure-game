0f8d219dd3e56ead8ac62259fbe70529ce50239a
Adrian-Kolodziejczyk <adriankolodziejczyk@proton.me>
2023-08-05 00:57:56 +0200

    Added damage and hit points calculation

--- main.py
+++ main.py
@@ -4,34 +4,68 @@ from os import system
 
 class Player:
     choices = ["travel", "stats", "rest", "quit"]
-    choice = 0
 
     def __init__(self):
         self.name = input("Welcome. What is your name?\n").capitalize()
         system("cls")
         self.archetype = input(
-            "Who would you like to become? Strong warrior, powerful mage or a stealthy rogue?\n"
+            "Who would you like to become?"
+            + "Strong warrior, powerful mage or a stealthy rogue?\n"
         ).lower()
         system("cls")
+        self.level = 1
         self.strength = 1
         self.intelligence = 1
         self.dexterity = 1
+        self.health = 1
+
+    @property
+    def min_damage(self):
+        match self.archetype:
+            case "warrior":
+                return self.strength
+            case "mage":
+                return self.intelligence
+            case "rogue":
+                return self.dexterity
+            case _:
+                return 0
+
+    @property
+    def max_damage(self):
+        match self.archetype:
+            case "warrior":
+                return self.strength * 2
+            case "mage":
+                return self.intelligence * 2
+            case "rogue":
+                return self.dexterity * 2
+            case _:
+                return 0
 
     @property
     def attack_damage(self):
-        if self.archetype == "warrior":
-            return self.strength * 2
-        elif self.archetype == "mage":
-            return self.intelligence * 2
-        elif self.archetype == "rogue":
-            return self.dexterity * 2
+        return f"{self.min_damage} - {self.max_damage}"
+
+    @property
+    def hit_points(self):
+        return 100 + 10 * (self.level - 1) + 10 * (self.health - 1)
 
     def __str__(self) -> str:
-        return f"Name: {self.name}  Class: {self.archetype.capitalize()}"
+        return (
+            f"\nName:             {self.name}"
+            + f"\nClass:            {self.archetype.capitalize()}"
+            + f"\nLevel:            {self.level}"
+        )
 
     def checkstats(self):
+        print(self)
         print(
-            f"\nStrength: {self.strength}\nIntelligence: {self.intelligence}\nDexterity: {self.dexterity}\nAttack: {self.attack_damage}"
+            f"\nStrength:         {self.strength}"
+            + f"\nIntelligence:     {self.intelligence}"
+            + f"\nDexterity:        {self.dexterity}"
+            + f"\n\nAttack damage:    {self.attack_damage}"
+            + f"\nHit points:       {self.hit_points}"
         )
 
     def addstats(self, stat, times=1):
@@ -50,8 +84,17 @@ class Player:
                 pass
         print(f"You have gained {times} point(s) of {stat}!")
 
+    def levelup(self):
+        self.level += 1
+        print(f"You have levelled up! You are now level {self.level}.")
+        self.addstats("every stats")
+
     def attack(self, target):
-        print(f"{target.name} watch out! You have been attacked by {self.name}!")
+        print(
+            f"{self.name} attacked {target.name}"
+            + f" and dealt {randint(self.min_damage,self.max_damage)}"
+        )
+        # todo
 
 
 class Enemy(Player):
@@ -64,30 +107,34 @@ class Enemy(Player):
 
 
 player = Player()
-# print(player)
-player.checkstats()
-player.addstats("strength")
-player.checkstats()
-player.addstats("intelligence", 2)
-player.checkstats()
-
-location = ["Dark woods", "Wetlands", "Magic ruins", "Bandit camp"]
+location = [
+    "Dark woods",
+    "Wetlands",
+    "Magic ruins",
+    "Bandit camp",
+    "Peaceful lands",
+    "Peaceful lands",
+    "Peaceful lands",
+    "Peaceful lands",
+    "Peaceful lands",
+    "Peaceful lands",
+    "Peaceful lands",
+]
 daycount = 1
 
 enemy = Enemy("Bandit", "warrior")
-enemy.attack(player)
 # enemy.checkstats()
 
 while True:
     match player.archetype:
         case "warrior":
-            player.strength += 3
+            player.strength += 2
             break
         case "mage":
-            player.intelligence += 3
+            player.intelligence += 2
             break
         case "rogue":
-            player.dexterity += 3
+            player.dexterity += 2
             break
         case _:
             player.archetype = input(
@@ -95,12 +142,20 @@ while True:
             )
 print(f"\nAh.. so {player.archetype} {player.name}!\nYour adventure begins now.\n")
 
+# print(player)
+# player.checkstats()
+# player.addstats("strength")
+# player.checkstats()
+# player.addstats("intelligence", 2)
+# player.checkstats()
+player.attack(enemy)
+
 while True:
     player.choice = input(
         f"\nDay: {daycount}\nWhat would you like to do? Travel, check stats or take a rest?\n"
     )
     if player.choice == player.choices[0]:
-        location_current = location[randint(0, 3)]
+        location_current = location[randint(0, len(location))]
         print(f"\nYou are now in {location_current}\n")
         if location_current == location[2]:
             print(
