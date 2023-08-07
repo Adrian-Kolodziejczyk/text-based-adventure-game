571d94767e51140a5cb247b5c62092a2b5af7da3
Adrian-Kolodziejczyk <adriankolodziejczyk@proton.me>
2023-08-07 23:01:53 +0200

    Imported new modules

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from entity.entity import Player
+from entity.player import Player
+from entity.enemy import Enemy
 from game.game import Game
+from game.items import Items
 
 
 if __name__ == "__main__":
